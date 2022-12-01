b90f8ff72b7d8b29b4f0a1479660cd3c218f733e
carlos-ventura <carlospinho_pt@hotmail.com>
2022-12-01 18:41:34 +0100

    Create initial Java Spring Boot template for REST API

---
+++ src/main/java/spring/boot/restapilog/RestApiLogApplication.java
@@ -0,0 +1,13 @@
+package spring.boot.restapilog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/boot/restapilog/RestApiLogApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.restapilog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
